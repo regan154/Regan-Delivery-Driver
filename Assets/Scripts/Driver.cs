b991e85d877ae431fd896f72ae1a8e1485fd21b2
Kris Regan <regan154@morrisville.edu>
2022-02-09 13:11:58 -0500

    Using Input.GetAxis()

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float fltSteerSpeed = 1f;
+    [SerializeField] float fltSteerSpeed = 0.8f;
     [SerializeField] float fltMoveSpeed = 0.01f;
     
     void Start()
@@ -14,7 +14,9 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, fltSteerSpeed);
-        transform.Translate(0, fltMoveSpeed, 0);
+        float fltSteerAmount = Input.GetAxis("Horizontal") * fltSteerSpeed;
+        float fltMoveAmount = Input.GetAxis("Vertical") * fltMoveSpeed;
+        transform.Rotate(0, 0, -fltSteerAmount);
+        transform.Translate(0, fltMoveAmount, 0);
     }
 }
