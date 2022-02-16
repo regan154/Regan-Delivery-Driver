ab0d6b5ca0e8e31ba363cb70f01ffb2f09d941dc
Kris Regan <regan154@morrisville.edu>
2022-02-16 12:59:47 -0500

    How to Use GetComponent

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     [SerializeField] float fltDestroyDelay = 0.5f;
     bool blHasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
            Debug.Log("Ouch!");
@@ -17,12 +28,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             blHasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, fltDestroyDelay);
         }
         if (other.tag == "Customer" && blHasPackage)
         {
             Debug.Log("Package Delivered");
             blHasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
